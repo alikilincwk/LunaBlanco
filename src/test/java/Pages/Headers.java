d4f751266368ab0dc7f34b694ec2a1157d5e0dec
betulgozlukoglu <gozlukoglubetul@gmail.com>
2025-03-11 17:29:28 +0300

    _05_StoreMenu Feature has been updated as Given,When,Then have been added Locaters for US_05 have been added to Headers Step Definition file has been added for US005

--- src/test/java/Pages/Headers.java
+++ src/test/java/Pages/Headers.java
@@ -2,12 +2,33 @@ package Pages;
 
 import Utilities.GWD;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import javax.xml.xpath.XPath;
 
 public class Headers extends ParentPage{
     public Headers() {
         PageFactory.initElements(GWD.getDriver(), this);
     }
+
+
+    //US_05_StoreMenu
+    @FindBy(linkText = "Store")
+    public WebElement store;
+    @FindBy(xpath="//*[text()='ALLE PIECES']")
+    public WebElement allPieces;
+    @FindBy(xpath="//*[text()='T-SHIRTS']")
+    public WebElement tShirts;
+    @FindBy(xpath="//*[text()='HOODIES']")
+    public WebElement hoodies;
+    @FindBy(xpath="//*[text()='SWEATER']")
+    public WebElement sweater;
+    @FindBy(xpath="//*[text()='HEADWEAR']")
+    public WebElement headwear;
+    @FindBy(xpath="//*[text()='ARCHIV']")
+    public WebElement archiv;
+
 
 
 
